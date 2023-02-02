b48b2dc2ad1a5415b32aaafbc6f3289c3c726901
dohyeon-scott-kim <dohyeon.scott.kim@gmail.com>
2023-02-02 18:26:16 +0900

    s-b-s 10

--- step_by_step/10/2750.py
+++ step_by_step/10/2750.py
@@ -8,7 +8,7 @@ for i in range(n):
 
 # https://modulabs.co.kr/blog/algorithm-python/
 
-# selection sort(선택정렬)) o(n^2)
+#selection sort(선택정렬)) o(n^2)
 # for i in range(n):
 #     minIndex = i
 #     for j in range(i+1, n):
@@ -30,16 +30,58 @@ for i in range(n):
 # for i in range(n):
 #     print(array[i])
 
-#bubble sort(버블정랼)
-for i in range(n):
-    for j in range(n-i-1):
-        if array[j] > array[j+1]:
-            array[j], array[j+1] = array[j+1], array[j]
+#bubble sort(버블정랼) o(n^2)
+# for i in range(n):
+#     for j in range(n-i-1):
+#         if array[j] > array[j+1]:
+#             array[j], array[j+1] = array[j+1], array[j]
+
+# for i in range(n):
+#     print(array[i])
+        
+#quick sort(퀵정렬) average:o(nlogn) worst: o(n^2)
+#quick sort - conventional method
+# def quicksort(array, start, end):
+#     if start >= end:
+#         return
+    
+#     pivot = start
+#     left = start+1
+#     right = end
+    
+#     while left <= right:
+        
+#         while left <= end and array[left] <= array[pivot]:
+#             left += 1
+        
+#         while right > start and array[right] >= array[pivot]:
+#             right -= 1
+        
+#         if left > right:
+#             array[right], array[pivot] = array[pivot], array[right]
+#         else:
+#             array[left], array[right] = array[right], arrya[left]
+    
+#     quicksort(array, start, right-1)
+#     quicksort(array, right+1, end)
+
+#quicksort - pythonic method
+def quicksort(array):
+    if len(array) <= 1:
+        return array
+    
+    pivot = array[0]
+    tail = array[1:]
+    
+    left = [i for i in tail if i <= pivot]
+    right = [i for i in tail if i > pivot]
+    
+    return quicksort(left) + [pivot] + quicksort(right)
+
+array = quicksort(array)
 
 for i in range(n):
     print(array[i])
-        
-#quick sort(퀵정렬)
 
 #merge sort(병합 정렬)
 
