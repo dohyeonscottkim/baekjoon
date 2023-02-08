4b315a5783d33fbca38a2c2c1ece5ef189329472
dohyeon-scott-kim <dohyeon.scott.kim@gmail.com>
2023-02-08 13:16:21 +0900

    s-b-s 10 heap sort

--- step_by_step/10/2750.py
+++ step_by_step/10/2750.py
@@ -4,9 +4,6 @@ array = []
 
 for i in range(n):
     array.append(int(input()))
-    
-
-# https://modulabs.co.kr/blog/algorithm-python/
 
 #selection sort(선택정렬)) o(n^2)
 # for i in range(n):
@@ -66,26 +63,92 @@ for i in range(n):
 #     quicksort(array, right+1, end)
 
 #quicksort - pythonic method
-def quicksort(array):
-    if len(array) <= 1:
-        return array
+# def quicksort(array):
+#     if len(array) <= 1:
+#         return array
     
-    pivot = array[0]
-    tail = array[1:]
+#     pivot = array[0]
+#     tail = array[1:]
     
-    left = [i for i in tail if i <= pivot]
-    right = [i for i in tail if i > pivot]
+#     left = [i for i in tail if i <= pivot]
+#     right = [i for i in tail if i > pivot]
     
-    return quicksort(left) + [pivot] + quicksort(right)
+#     return quicksort(left) + [pivot] + quicksort(right)
 
-array = quicksort(array)
+# array = quicksort(array)
 
-for i in range(n):
-    print(array[i])
+# for i in range(n):
+#     print(array[i])
 
 #merge sort(병합 정렬)
+# def mergeSort(array:list):
+#     if len(array) > 1:
+#         leftArray = array[:len(array)//2]
+#         rightArray = array[len(array)//2:]
+        
+#         mergeSort(leftArray)
+#         mergeSort(rightArray)
+        
+#         l = 0
+#         r = 0
+#         m = 0
+        
+#         while l < len(leftArray) and r < len(rightArray):
+#             if leftArray[l] < rightArray[r]:
+#                 array[m] = leftArray[l]
+#                 l += 1
+#             else:
+#                 array[m] = rightArray[r]
+#                 r += 1
+#             m += 1
+        
+#         while l < len(leftArray):
+#             array[m] = leftArray[l]
+#             l += 1
+#             m += 1
+        
+#         while r < len(rightArray):
+#             array[m] = rightArray[r]
+#             r += 1
+#             m += 1
+            
+# mergeSort(array)
+
+# for i in range(n):
+#     print(array[i])
 
 #heap sort(힙 정렬)
+def heapify(arr, idx, size):
+    left = idx*2
+    right = idx*2 + 1
+    largest = idx
+    
+    if left < size and arr[left] > arr[largest]:
+        largest = left
+    
+    if right < size and arr[right] > arr[largest]:
+        largest = right
+    
+    if largest != idx:
+        arr[idx], arr[largest] = arr[largest], arr[idx]
+        heapify(arr, largest, size)
+        
+def heapsort(arr):
+    size = len(arr)
+    
+    for i in range(size//2-1, -1, -1):
+        heapify(arr, i, size)
+    
+    for i in range(n-1, 0, -1):
+        arr[0], arr[i] = arr[i], arr[0]
+        heapify(arr, 0, i)
+    
+    return arr
+
+sortedArray = heapsort(array)
+
+for i in range(n):
+    print(sortedArray[i])
 
 #radix sort(기수 정렬)
 
